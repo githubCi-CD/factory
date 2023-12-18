15a8d0304b85568d468bea82a734f76dd23cc2f4
tooli38 <tooli38@gmail.com>
2023-12-18 20:29:24 +0900

    factory 필드명 변경

--- src/main/kotlin/spring/factotry/model/Factory.kt
+++ src/main/kotlin/spring/factotry/model/Factory.kt
@@ -11,7 +11,7 @@ data class Factory(
     val income: Long = 0,
     val outcome: Long = 0,
     val asset: Long = 50000000,
-    val totalCount: Int = 0,
+    val failureCount: Int = 0,
     val successCount: Int = 0 ,
     val status: Boolean = true,
 )
